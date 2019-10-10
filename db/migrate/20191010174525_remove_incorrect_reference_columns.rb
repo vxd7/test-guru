2df71cc78d9fd039885846791a7408efac98b6de
vxd7 <ykonpaku13@gmail.com>
2019-10-10 21:22:07 +0300

    Added removal of category_id from tests table

--- db/migrate/20191010174525_remove_incorrect_reference_columns.rb
+++ db/migrate/20191010174525_remove_incorrect_reference_columns.rb
@@ -2,5 +2,6 @@ class RemoveIncorrectReferenceColumns < ActiveRecord::Migration[6.0]
   def change
     remove_column(:answers, :question_id, :integer)
     remove_column(:questions, :test_id, :integer)
+    remove_column(:tests, :category_id, :integer)
   end
 end
