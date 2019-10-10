b80f26c252eafaa203c2a6de37fe6ea80f3580f4
vxd7 <ykonpaku13@gmail.com>
2019-10-10 19:49:51 +0300

    Reverted all custom migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,40 +10,38 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_09_222156) do
+ActiveRecord::Schema.define(version: 2019_10_09_212708) do
 
   create_table "answers", force: :cascade do |t|
     t.string "content"
-    t.integer "question_id", null: false
-    t.integer "user_id", null: false
+    t.integer "question_id"
+    t.integer "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.boolean "correct", default: false, null: false
   end
 
   create_table "categories", force: :cascade do |t|
-    t.string "name", null: false
+    t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "questions", force: :cascade do |t|
-    t.string "content", null: false
-    t.integer "test_id", null: false
+    t.string "content"
+    t.integer "test_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.integer "category"
   end
 
   create_table "tests", force: :cascade do |t|
-    t.string "title", null: false
-    t.integer "level", default: 0, null: false
+    t.string "title"
+    t.integer "level", default: -1
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "name", null: false
+    t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
