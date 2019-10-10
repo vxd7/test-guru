8cd150ca5403f05130ae604ade48bf7328ca639a
vxd7 <ykonpaku13@gmail.com>
2019-10-10 20:21:21 +0300

    Applied migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_09_212708) do
+ActiveRecord::Schema.define(version: 2019_10_10_171853) do
 
   create_table "answers", force: :cascade do |t|
     t.string "content"
     t.integer "question_id"
-    t.integer "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
@@ -38,6 +37,7 @@ ActiveRecord::Schema.define(version: 2019_10_09_212708) do
     t.integer "level", default: -1
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "category_id"
   end
 
   create_table "users", force: :cascade do |t|
