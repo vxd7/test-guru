6c5c7a8cac139e330ce05a8d25b0273517ce24d9
vxd7 <ykonpaku13@gmail.com>
2019-10-10 20:57:05 +0300

    Bugfix in naming

--- db/migrate/20191010174525_remove_incorrect_reference_columns.rb
+++ db/migrate/20191010174525_remove_incorrect_reference_columns.rb
@@ -1,6 +1,6 @@
 class RemoveIncorrectReferenceColumns < ActiveRecord::Migration[6.0]
   def change
     remove_column(:answers, :question_id, :integer)
-    remove_column(:questinos, :test_id, :integer)
+    remove_column(:questions, :test_id, :integer)
   end
 end
