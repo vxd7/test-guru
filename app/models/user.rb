e428207e98b5db1d4d8938976b35115582226c1f
vxd7 <ykonpaku13@gmail.com>
2019-10-13 01:40:38 +0300

    Added function to select user tests of particular level

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+  def user_tests_by_level(level)
+    tests = Test.joins('JOIN user_tests ON user_tests.test_id = tests.id')
+    tests.where('user_tests.user_id = :user_id AND tests.level = :test_level',
+                user_id: id, test_level: level)
+  end
 end
