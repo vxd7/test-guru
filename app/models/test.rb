1df931ac5361017a37e03b4405ff93272324ea77
vxd7 <ykonpaku13@gmail.com>
2019-10-13 01:59:25 +0300

    Modified tests_by_category to return array of test titles

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,6 +1,10 @@
 class Test < ApplicationRecord
   def self.tests_by_category(category)
     tests = Test.joins('JOIN categories ON tests.category_id = categories.id')
-    tests.where('categories.name = :cat_name', cat_name: category).order('id DESC')
+    tests = tests.where('categories.name = :cat_name', cat_name: category).order('tests.id DESC').select(:title)
+
+    res = []
+    tests.each { |test| res << test.title }
+    res
   end
 end
