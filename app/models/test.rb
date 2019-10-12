12a9103cc78365e59d37f6f7219f88237a59caf8
vxd7 <ykonpaku13@gmail.com>
2019-10-13 01:51:27 +0300

    Added class method to find tests by category

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,2 +1,6 @@
 class Test < ApplicationRecord
+  def self.tests_by_category(category)
+    tests = Test.joins('JOIN categories ON tests.category_id = categories.id')
+    tests.where('categories.name = :cat_name', cat_name: category).order('id DESC')
+  end
 end
