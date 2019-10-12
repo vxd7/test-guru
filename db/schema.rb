854f8dd32b16fba9b8843b56c187f5a59782510b
vxd7 <ykonpaku13@gmail.com>
2019-10-13 00:43:37 +0300

    Applied the migration to create previously defined table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_10_181937) do
+ActiveRecord::Schema.define(version: 2019_10_12_213615) do
 
   create_table "answers", force: :cascade do |t|
     t.string "content"
@@ -44,6 +44,15 @@ ActiveRecord::Schema.define(version: 2019_10_10_181937) do
     t.index ["category_id"], name: "index_tests_on_category_id"
   end
 
+  create_table "user_tests", force: :cascade do |t|
+    t.integer "test_id", null: false
+    t.integer "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["test_id"], name: "index_user_tests_on_test_id"
+    t.index ["user_id"], name: "index_user_tests_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "name", null: false
     t.datetime "created_at", precision: 6, null: false
@@ -53,4 +62,6 @@ ActiveRecord::Schema.define(version: 2019_10_10_181937) do
   add_foreign_key "answers", "questions"
   add_foreign_key "questions", "tests"
   add_foreign_key "tests", "categories"
+  add_foreign_key "user_tests", "tests"
+  add_foreign_key "user_tests", "users"
 end
