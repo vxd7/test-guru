9ed403fb03bbf38e7e7b49e789ab33f5ecec2741
vxd7 <ykonpaku13@gmail.com>
2019-10-13 17:33:21 +0300

    Fixed more codestyle issues in tests_by_category

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,7 +1,7 @@
 class Test < ApplicationRecord
   def self.tests_by_category(category)
     joins('JOIN categories ON tests.category_id = categories.id')
-      .where('categories.name = :cat_name', cat_name: category)
+      .where(categories: { name: category })
       .order('tests.id DESC')
       .pluck(:title)
   end
