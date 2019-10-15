24393a63a04b902e88831c18c65906cefb60372b
vxd7 <ykonpaku13@gmail.com>
2019-10-15 18:18:15 +0300

    Changed user_tests_by_level method of User class to use associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,8 @@ class User < ApplicationRecord
   has_many :tests, through: :user_tests, dependent: :destroy
 
   def user_tests_by_level(level)
-    Test.joins('JOIN user_tests ON user_tests.test_id = tests.id')
+    # Test.joins('JOIN user_tests ON user_tests.test_id = tests.id')
+    Test.joins(:user_tests)
         .where(user_tests: { user_id: id }, level: level)
   end
 end
