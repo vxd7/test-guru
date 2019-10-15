595b058af170567291a710103b79fa9ece4aa506
vxd7 <ykonpaku13@gmail.com>
2019-10-15 16:35:36 +0300

    Changed association between author and test to include authored_tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   # User can be an author of many tests
-  has_many :tests, foreign_key: :author_id
+  has_many :authored_tests, class_name: :Test, foreign_key: :author_id
   has_many :user_tests
   has_many :tests, through: :user_tests
 
