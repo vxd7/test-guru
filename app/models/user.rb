59d13797fbdd76443da6efea626c8d2c3cc091da
vxd7 <ykonpaku13@gmail.com>
2019-10-17 05:29:21 +0300

    Somplified user_tests_by_level by using associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,8 @@ class User < ApplicationRecord
 
   def user_tests_by_level(level)
     # Test.joins('JOIN user_tests ON user_tests.test_id = tests.id')
-    Test.joins(:user_tests)
+    # Test.joins(:user_tests)
+    Test.joins(:users)
         .where(user_tests: { user_id: id }, level: level)
   end
 end
