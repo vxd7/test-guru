573f00a0c90e06aa5d5216df4ed20742d7ead948
vxd7 <ykonpaku13@gmail.com>
2019-10-17 05:06:27 +0300

    Applied changed migration

--- db/schema.rb
+++ db/schema.rb
@@ -41,7 +41,7 @@ ActiveRecord::Schema.define(version: 2019_10_15_015736) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "category_id"
-    t.integer "author_id", null: false
+    t.integer "author_id"
     t.index ["author_id"], name: "index_tests_on_author_id"
     t.index ["category_id"], name: "index_tests_on_category_id"
   end
