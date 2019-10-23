6964806d77e81dcf83cc2d996a72e1f102d1da4b
vxd7 <ykonpaku13@gmail.com>
2019-10-23 05:48:10 +0300

    Typo fix

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   # get 'about/author'
   resources :tests do
-    resources :questoins, shallow: true
+    resources :questions, shallow: true
   end
 end
