67ac09d94098d05dc900888637ad576d8de7415f
vxd7 <ykonpaku13@gmail.com>
2019-10-23 05:25:22 +0300

    Added questions and tests resources in routes file

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get 'about/author'
+  # get 'about/author'
+  resources :tests do
+    resources :questoins, shallow: true
+  end
 end
