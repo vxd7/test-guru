124de057fa8a96992b75d72d437fff4e379a746b
vxd7 <ykonpaku13@gmail.com>
2019-10-23 05:53:43 +0300

    Added viewing of one question

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,13 +1,22 @@
 class QuestionsController < ApplicationController
-  before_action :find_test
+  before_action :find_test, only: %i[index create new]
+  before_action :find_question, only: %i[edit show update destroy]
 
   def index
     render plain: @test.questions.inspect
+  end
+
+  def show
+    render plain: @question.inspect
   end
 
   private
 
   def find_test
     @test = Test.find(params[:test_id])
+  end
+
+  def find_question
+    @question = Question.find(params[:id])
   end
 end
