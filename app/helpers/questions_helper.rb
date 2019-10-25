28ad70b09635fe51886f4f41516e34bba7590b13
vxd7 <ykonpaku13@gmail.com>
2019-10-25 04:34:26 +0300

    Added question_header helper

--- app/helpers/questions_helper.rb
+++ app/helpers/questions_helper.rb
@@ -1,2 +1,10 @@
 module QuestionsHelper
+  def question_header(action, test)
+    case action
+    when 'create'
+      "Create new '#{test.title}' question"
+    when 'edit'
+      "Edit '#{test.title}' question"
+    end
+  end
 end
