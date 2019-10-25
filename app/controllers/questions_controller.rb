6a3c698d5afd22626a34d7101d986d7b67bb7452
vxd7 <ykonpaku13@gmail.com>
2019-10-25 03:56:53 +0300

    Added view for rendering of all questions of particular test

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -5,7 +5,7 @@ class QuestionsController < ApplicationController
   rescue_from ActiveRecord::RecordNotFound, with: :rescue_with_question_not_found
 
   def index
-    render plain: @test.questions.inspect
+    @questions = @test.questions
   end
 
   def show
