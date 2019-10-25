a34126c1691599d378bce2e5d170579a90d149ab
vxd7 <ykonpaku13@gmail.com>
2019-10-25 04:16:42 +0300

    Moved rendering of questions into shared partial and added list of all questions to the test page

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -4,9 +4,7 @@ class QuestionsController < ApplicationController
 
   rescue_from ActiveRecord::RecordNotFound, with: :rescue_with_question_not_found
 
-  def index
-    @questions = @test.questions
-  end
+  def index; end
 
   def show
     render plain: @question.inspect
