ddb8bf7704d98863fcf4a536974591d9a9f72630
vxd7 <ykonpaku13@gmail.com>
2019-10-26 03:50:54 +0300

    Added show view for question and changed question_controller

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -4,9 +4,7 @@ class QuestionsController < ApplicationController
 
   rescue_from ActiveRecord::RecordNotFound, with: :rescue_with_question_not_found
 
-  def show
-    render plain: @question.inspect
-  end
+  def show; end
 
   def new
     @question = @test.questions.new
