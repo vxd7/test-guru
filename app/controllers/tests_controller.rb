dbc560afdd4df00319f5dace86569e3d56816d01
vxd7 <ykonpaku13@gmail.com>
2019-10-26 05:00:27 +0300

    Added new test action, views and helper

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -6,4 +6,24 @@ class TestsController < ApplicationController
   def show
     @test = Test.find(params[:id])
   end
+
+  def new
+    @test = Test.new
+  end
+
+  def create
+    @test = Test.create(test_params)
+
+    if @test.save
+      redirect_to @test
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def test_params
+    params.require(:test).permit(:title, :level, :category)
+  end
 end
--- app/helpers/tests_helper.rb
+++ app/helpers/tests_helper.rb
@@ -1,2 +1,9 @@
 module TestsHelper
+  def test_header(test)
+    if test.new_record?
+      'Create new test'
+    elsif test.persisted?
+      "Edit test '#{test.title}'"
+    end
+  end
 end
