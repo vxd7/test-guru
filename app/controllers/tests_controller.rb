68a83ca34b70aaa7dbbac1874fb87cc400463999
vxd7 <ykonpaku13@gmail.com>
2019-10-26 05:06:39 +0300

    Added test editing feature

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -1,11 +1,11 @@
 class TestsController < ApplicationController
+  before_action :find_test, only: %i[show edit update destroy]
+
   def index
     @tests = Test.all
   end
 
-  def show
-    @test = Test.find(params[:id])
-  end
+  def show; end
 
   def new
     @test = Test.new
@@ -21,9 +21,23 @@ class TestsController < ApplicationController
     end
   end
 
+  def edit; end
+
+  def update
+    if @test.update(test_params)
+      redirect_to @test
+    else
+      render :edit
+    end
+  end
+
   private
 
   def test_params
     params.require(:test).permit(:title, :level, :category)
+  end
+
+  def find_test
+    @test = Test.find(params[:id])
   end
 end
