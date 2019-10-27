7f1ffe4ce69d338d485754e9ea8a5d6d07f45b01
vxd7 <ykonpaku13@gmail.com>
2019-10-27 19:59:53 +0300

    Added route for test_passages

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,14 @@ Rails.application.routes.draw do
   # get 'about/author'
   resources :tests do
     resources :questions, shallow: true, except: :index do
-      resources :answers, shallow: true
+      resources :answers, shallow: true, except: :index
+    end
+  end
+
+  # GET /test_passages/:id/result
+  resources :test_passages, only: %i[show update] do
+    member do
+      get :result
     end
   end
 end
