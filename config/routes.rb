3bc7955ad404e381365e5242518bbeaa6a208775
vxd7 <ykonpaku13@gmail.com>
2019-10-27 18:45:29 +0300

    Added answer resource to routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   # get 'about/author'
   resources :tests do
-    resources :questions, shallow: true, except: :index
+    resources :questions, shallow: true, except: :index do
+      resources :answers, shallow: true
+    end
   end
 end
