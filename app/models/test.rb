6715cc8969c90eb3287e48d59eb74bb6fc7a143a
vxd7 <ykonpaku13@gmail.com>
2019-10-27 19:43:42 +0300

    Changed user test association to use test_passages table instead of user_tests

--- app/models/test.rb
+++ app/models/test.rb
@@ -8,8 +8,8 @@ class Test < ApplicationRecord
   belongs_to :author, class_name: :User, foreign_key: :author_id, optional: true
   belongs_to :category, optional: true
 
-  has_many :user_tests, dependent: :destroy
-  has_many :users, through: :user_tests
+  has_many :test_passages, dependent: :destroy
+  has_many :users, through: :test_passages
   has_many :questions, dependent: :destroy
 
   validates :title, presence: true, uniqueness: { scope: :level, message: 'can be only one test with given title and level' }
