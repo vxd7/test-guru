04331a011e4fad6ab110daf1432c69f81968161e
vxd7 <ykonpaku13@gmail.com>
2019-10-27 19:41:52 +0300

    Changed user test association to use test_passages table instead of user_tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,15 +2,15 @@ class User < ApplicationRecord
   # User can be an author of many tests
   has_many :authored_tests, class_name: :Test, foreign_key: :author_id,
                             dependent: :nullify
-  has_many :user_tests, dependent: :destroy
-  has_many :tests, through: :user_tests
+  has_many :test_passages, dependent: :destroy
+  has_many :tests, through: :test_passages
 
   validates :email, presence: true
 
   def user_tests_by_level(level)
-    # Test.joins('JOIN user_tests ON user_tests.test_id = tests.id')
-    # Test.joins(:user_tests)
+    # Test.joins('JOIN test_passages ON test_passages.test_id = tests.id')
+    # Test.joins(:test_passages)
     Test.joins(:users)
-        .where(user_tests: { user_id: id }, level: level)
+        .where(test_passages: { user_id: id }, level: level)
   end
 end
