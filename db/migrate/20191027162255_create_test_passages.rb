cd71d0acb6b8f043763cbf44ea8233ff4e921215
vxd7 <ykonpaku13@gmail.com>
2019-10-27 19:38:27 +0300

    Changed generated migration to include default value

--- db/migrate/20191027162255_create_test_passages.rb
+++ db/migrate/20191027162255_create_test_passages.rb
@@ -4,7 +4,7 @@ class CreateTestPassages < ActiveRecord::Migration[6.0]
       t.references :user, null: false, foreign_key: true
       t.references :test, null: false, foreign_key: true
       t.references :current_question, null: false, foreign_key: true
-      t.integer :correct_question
+      t.integer :correct_question, default: 0
 
       t.timestamps
     end
