c0bbd3d210d1134569dfa54c5ba45eeb6ea3b774
vxd7 <ykonpaku13@gmail.com>
2019-10-27 20:10:39 +0300

    Added test_passage method to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,4 +13,8 @@ class User < ApplicationRecord
     Test.joins(:users)
         .where(test_passages: { user_id: id }, level: level)
   end
+
+  def test_passage(test)
+    test_passages.order(id: :desc).find_by(test_id: test.id)
+  end
 end
