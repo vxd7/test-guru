1c264f068eedb909bb88df091547f98852743743
vxd7 <ykonpaku13@gmail.com>
2019-10-27 18:45:11 +0300

    Changed answer controller

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,74 +1,54 @@
 class AnswersController < ApplicationController
-  before_action :set_answer, only: [:show, :edit, :update, :destroy]
+  before_action :find_question, only: %i[new create]
+  before_action :set_answer, only: %i[show edit update destroy]
 
-  # GET /answers
-  # GET /answers.json
-  def index
-    @answers = Answer.all
-  end
-
-  # GET /answers/1
-  # GET /answers/1.json
   def show
   end
 
-  # GET /answers/new
   def new
-    @answer = Answer.new
+    @answer = @question.answers.new
   end
 
-  # GET /answers/1/edit
   def edit
   end
 
-  # POST /answers
-  # POST /answers.json
   def create
-    @answer = Answer.new(answer_params)
+    @answer = @question.answers.new(answer_params)
 
-    respond_to do |format|
-      if @answer.save
-        format.html { redirect_to @answer, notice: 'Answer was successfully created.' }
-        format.json { render :show, status: :created, location: @answer }
-      else
-        format.html { render :new }
-        format.json { render json: @answer.errors, status: :unprocessable_entity }
-      end
+    if @answer.save
+      redirect_to @answer
+    else
+      render :new
     end
   end
 
-  # PATCH/PUT /answers/1
-  # PATCH/PUT /answers/1.json
   def update
-    respond_to do |format|
-      if @answer.update(answer_params)
-        format.html { redirect_to @answer, notice: 'Answer was successfully updated.' }
-        format.json { render :show, status: :ok, location: @answer }
-      else
-        format.html { render :edit }
-        format.json { render json: @answer.errors, status: :unprocessable_entity }
-      end
+    if @answer.update(answer_params)
+      redirect_to @answer
+    else
+      render :edit
     end
   end
 
-  # DELETE /answers/1
-  # DELETE /answers/1.json
   def destroy
     @answer.destroy
-    respond_to do |format|
-      format.html { redirect_to answers_url, notice: 'Answer was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+
+    redirect_to @answer.question
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_answer
-      @answer = Answer.find(params[:id])
-    end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def answer_params
-      params.fetch(:answer, {})
-    end
+  def find_question
+    @question = Question.find(params[:question_id])
+  end
+
+  # Use callbacks to share common setup or constraints between actions.
+  def set_answer
+    @answer = Answer.find(params[:id])
+  end
+
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def answer_params
+    params.require(:answer).permit(:content, :correct)
+  end
 end
