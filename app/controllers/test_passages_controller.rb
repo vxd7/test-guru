231f982df7f460e3f52cd89c5bf64f6e434086b2
vxd7 <ykonpaku13@gmail.com>
2019-10-28 05:59:20 +0300

    Changed route helper path method name

--- app/controllers/test_passages_controller.rb
+++ app/controllers/test_passages_controller.rb
@@ -12,7 +12,7 @@ class TestPassagesController < ApplicationController
     @test_passage.accept!(params[:answer_ids])
 
     if @test_passage.completed?
-      redirect_to test_passage_result_path(@test_passage)
+      redirect_to result_test_passage_path(@test_passage)
     else
       render :show
     end
