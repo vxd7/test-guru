bb07808400dc172c6b4c923d035ec83a7a2428ad
vxd7 <ykonpaku13@gmail.com>
2019-10-28 05:29:03 +0300

    Rendering test passage done

--- db/schema.rb
+++ db/schema.rb
@@ -67,7 +67,7 @@ ActiveRecord::Schema.define(version: 2019_10_27_164436) do
 
   add_foreign_key "answers", "questions"
   add_foreign_key "questions", "tests"
-  add_foreign_key "test_passages", "current_questions"
+  add_foreign_key "test_passages", "questions", column: "current_question_id"
   add_foreign_key "test_passages", "tests"
   add_foreign_key "test_passages", "users"
   add_foreign_key "tests", "categories"
