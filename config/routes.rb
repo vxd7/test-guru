f66aeb52a7bf134431c639d9c6fcaf18a5ad303a
vxd7 <ykonpaku13@gmail.com>
2019-10-31 22:27:11 +0300

    Added routes for user create and signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   get 'users/new'
+
+  get :signup, to: 'users#new'
+  resources :users, only: :create
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   # get 'about/author'
   resources :tests do
