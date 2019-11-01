3f7874cfaec3c0df693689790f4390b8a46b001c
vxd7 <ykonpaku13@gmail.com>
2019-11-01 05:26:01 +0300

    Moved flash_message to app helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,8 @@ module ApplicationHelper
   def github_url(author, project_name)
     link_to(project_name, "http://github.com/#{author}/#{project_name}")
   end
+
+  def flash_message(msg_level)
+    content_tag(:p, flash[msg_level], class: 'flash alert') if flash[msg_level]
+  end
 end
