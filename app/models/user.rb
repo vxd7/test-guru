eb16765b2e9f40ca9c699a5db6d2c2b7ca0bc3df
vxd7 <ykonpaku13@gmail.com>
2019-11-03 00:56:33 +0300

    Generated devise for User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
   # User can be an author of many tests
   has_many :authored_tests, class_name: :Test, foreign_key: :author_id,
                             dependent: :nullify
