167b794a9c3bd1a2b49be4bbe28e5ed8047e67f1
vxd7 <ykonpaku13@gmail.com>
2019-11-04 00:13:43 +0300

    Removed tests modification actions from user's test controller

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -1,5 +1,5 @@
 class TestsController < ApplicationController
-  before_action :find_test, only: %i[show edit update destroy start]
+  before_action :find_test, only: %i[start]
   # before_action :set_user, only: :start
 
   def index
@@ -8,36 +8,6 @@ class TestsController < ApplicationController
 
   def show; end
 
-  def new
-    @test = Test.new
-  end
-
-  def create
-    @test = Test.create(test_params)
-
-    if @test.save
-      redirect_to @test
-    else
-      render :new
-    end
-  end
-
-  def edit; end
-
-  def update
-    if @test.update(test_params)
-      redirect_to @test
-    else
-      render :edit
-    end
-  end
-
-  def destroy
-    @test.destroy
-
-    redirect_to tests_path
-  end
-
   def start
     user = current_user
     user.tests.push(@test)
@@ -45,10 +15,6 @@ class TestsController < ApplicationController
   end
 
   private
-
-  def test_params
-    params.require(:test).permit(:title, :level, :category_id)
-  end
 
   def find_test
     @test = Test.find(params[:id])
