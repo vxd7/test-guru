4c5a874d8c3f641394f8db5008220064e824a91a
vxd7 <ykonpaku13@gmail.com>
2019-11-04 20:01:50 +0300

    Modified SessionsController to redefine create action

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,9 +1,6 @@
 class SessionsController < Devise::SessionsController
-  after_action :show_flash, only: :create
-
-  private
-
-  def show_flash
+  def create
+    super
     flash[:notice] = "Hello, #{current_user.full_name}!"
   end
 end
