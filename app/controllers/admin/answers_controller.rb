05313ef6245e9f5e0267ad8d3a9816dc4366c3da
vxd7 <ykonpaku13@gmail.com>
2019-11-04 05:31:33 +0300

    More questions and answers link fixing

--- app/controllers/admin/answers_controller.rb
+++ app/controllers/admin/answers_controller.rb
@@ -16,7 +16,7 @@ class Admin::AnswersController < Admin::BaseController
     @answer = @question.answers.new(answer_params)
 
     if @answer.save
-      redirect_to admin_answer(@answer)
+      redirect_to admin_answer_path(@answer)
     else
       render :new
     end
@@ -24,7 +24,7 @@ class Admin::AnswersController < Admin::BaseController
 
   def update
     if @answer.update(answer_params)
-      redirect_to admin_answer(@answer)
+      redirect_to admin_answer_path(@answer)
     else
       render :edit
     end
@@ -33,7 +33,7 @@ class Admin::AnswersController < Admin::BaseController
   def destroy
     @answer.destroy
 
-    redirect_to admin_question(@answer.question)
+    redirect_to admin_question_path(@answer.question)
   end
 
   private
