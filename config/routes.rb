a81993cab3ef9ba2df4d898d3f71669bde8292f5
vxd7 <ykonpaku13@gmail.com>
2019-11-04 19:43:46 +0300

    Removed unnecessary user routes

--- config/routes.rb
+++ config/routes.rb
@@ -6,24 +6,10 @@ Rails.application.routes.draw do
   get 'sessions/new'
   get 'users/new'
 
-  # get :signup, to: 'users#new'
-  # get :login, to: 'sessions#new'
-  # delete :logout, to: 'sessions#destroy'
-
-  # resources :users, only: :create
-  # resources :sessions, only: :create
-
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   # get 'about/author'
   resources :tests, only: :index do
-    resources :questions, shallow: true, except: :index do
-      resources :answers, shallow: true, except: :index
-    end
-
     post :start, on: :member
-    # member do
-    #   post :start
-    # end
   end
 
   # GET /test_passages/:id/result
