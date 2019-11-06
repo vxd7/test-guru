bd13e8ff96011d116a4605f2f5c8f4f307948684
vxd7 <ykonpaku13@gmail.com>
2019-11-07 01:55:40 +0300

    Minor codestyle fixes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,11 +2,11 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
 
-  def after_sign_in_path_for(resource)
-    if resource.type == 'Admin'
+  def after_sign_in_path_for(user)
+    if user.admin?
       admin_tests_path
     else
-      super(resource)
+      super(user)
     end
   end
 
