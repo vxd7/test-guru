6303533089e5a155d6e7de25180b1ad2e5b220a3
vxd7 <ykonpaku13@gmail.com>
2019-11-07 16:58:20 +0300

    Changed views and controllers to use new gists route

--- app/controllers/test_passages_controller.rb
+++ app/controllers/test_passages_controller.rb
@@ -1,5 +1,5 @@
 class TestPassagesController < ApplicationController
-  before_action :set_test_passage, only: %i[show update result]
+  before_action :set_test_passage, only: %i[show update result gist]
 
   def show
 
@@ -16,6 +16,18 @@ class TestPassagesController < ApplicationController
     else
       render :show
     end
+  end
+
+  def gist
+    result = GistQuestionService.new(@test_passage.current_question).call
+
+    flash_options = if result.success?
+                      { notice: t('.success') }
+                    else
+                      { notice: t('.failure') }
+                    end
+
+    redirect_to @test_passage, flash_options
   end
 
   private
