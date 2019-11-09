5911ea703a752d1e67c511cb54974187fac245ee
vxd7 <ykonpaku13@gmail.com>
2019-11-09 20:46:05 +0300

    Added sorting js script

---
+++ app/javascript/utilities/sorting.js
@@ -0,0 +1,6 @@
+document.addEventListener('turbolinks:load', function() {
+  var control = document.querySelector('.sort-by-title')
+  control.addEventListener('click', function() {
+    alert('Click')
+  })
+})
