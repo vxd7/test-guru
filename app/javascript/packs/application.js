46224103b7f842f63778bba29849866f4cfee85c
vxd7 <ykonpaku13@gmail.com>
2019-11-09 23:36:37 +0300

    Added password confirmation correctness icons

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,7 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 require("utilities/sorting")
+require("utilities/password_confirmation")
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
