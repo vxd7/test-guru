dd12a64d3e03114c83ae6eaf020d4f528187d335
vxd7 <ykonpaku13@gmail.com>
2019-11-11 02:22:45 +0300

    Added update_inline method

--- app/controllers/admin/tests_controller.rb
+++ app/controllers/admin/tests_controller.rb
@@ -1,9 +1,8 @@
 class Admin::TestsController < Admin::BaseController
-  before_action :find_test, only: %i[show edit update destroy] # start
+  before_action :find_tests, only: %i[update_inline index]
+  before_action :find_test, only: %i[show edit update destroy update_inline] # start
 
-  def index
-    @tests = Test.all
-  end
+  def index; end
 
   def show; end
 
@@ -31,6 +30,16 @@ class Admin::TestsController < Admin::BaseController
     end
   end
 
+  # Update test title in admin table
+  # inline
+  def update_inline
+    if @test.update(test_params)
+      redirect_to admin_tests_path
+    else
+      render :index
+    end
+  end
+
   def destroy
     @test.destroy
 
@@ -38,6 +47,10 @@ class Admin::TestsController < Admin::BaseController
   end
 
   private
+
+  def find_tests
+    @tests = Test.all
+  end
 
   def test_params
     params.require(:test).permit(:title, :level, :category_id)
