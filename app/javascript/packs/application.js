b829e42ac2a4d69625de00ab3b645d3c02fdbeee
vxd7 <ykonpaku13@gmail.com>
2019-11-11 03:37:28 +0300

    Added js code to rub inline forms

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,6 +9,7 @@ require("@rails/activestorage").start()
 require("channels")
 require("utilities/sorting")
 require("utilities/password_confirmation")
+require("utilities/form_inline")
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
