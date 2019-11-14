6110f0cf5623d9b965b99c97cca105e9a9d823a0
vxd7 <ykonpaku13@gmail.com>
2019-11-15 00:16:10 +0300

    Added views and helpers for badge creation

--- app/helpers/badges_helper.rb
+++ app/helpers/badges_helper.rb
@@ -1,2 +1,11 @@
 module BadgesHelper
+  BADGE_IMAGES = Dir.glob("#{Rails.root}/public/images/badges/*").map do |p|
+    Pathname(p).basename.to_s
+  end
+
+  def avaliable_pictures
+    BADGE_IMAGES.map do |img_name|
+      File.join('badges', img_name)
+    end
+  end
 end
