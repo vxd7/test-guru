ee65ecc7ac31da192cc8a0343f16d79faa3a8d13
vxd7 <ykonpaku13@gmail.com>
2019-11-16 20:56:15 +0300

    Bugfix: check rule uniqueness in Badge

--- app/models/badge.rb
+++ app/models/badge.rb
@@ -1,4 +1,5 @@
 class Badge < ApplicationRecord
   has_many :user_badges, dependent: :nullify
   has_many :users, through: :user_badges
+  validates :rule, presence: true, uniqueness: { message: 'rule should be unique' }
 end
