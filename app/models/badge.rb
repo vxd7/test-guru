828dbba0df0fb00739fa02e621b541f7f0c0842e
vxd7 <ykonpaku13@gmail.com>
2019-11-17 18:59:39 +0300

    Removed badge rule uniqueness validation

--- app/models/badge.rb
+++ app/models/badge.rb
@@ -1,5 +1,4 @@
 class Badge < ApplicationRecord
   has_many :user_badges, dependent: :nullify
   has_many :users, through: :user_badges
-  validates :rule, presence: true, uniqueness: { message: 'rule should be unique' }
 end
