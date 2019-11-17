bac7335953b8ff737e5945888a55cb2f1e816f4d
vxd7 <ykonpaku13@gmail.com>
2019-11-17 16:13:40 +0300

    Added value column to badges

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_15_230733) do
+ActiveRecord::Schema.define(version: 2019_11_17_131252) do
 
   create_table "answers", force: :cascade do |t|
     t.string "content"
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 2019_11_15_230733) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "description"
+    t.string "value"
   end
 
   create_table "categories", force: :cascade do |t|
