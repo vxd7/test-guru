c0c026af57aa293423e8ff4cb8bd279c7c1a8bf5
vxd7 <ykonpaku13@gmail.com>
2019-11-18 18:45:36 +0300

    Added badge Rule-Value validation
    And also added translations for this validation

--- app/models/badge.rb
+++ app/models/badge.rb
@@ -2,5 +2,6 @@ class Badge < ApplicationRecord
   has_many :user_badges, dependent: :nullify
   has_many :users, through: :user_badges
 
-  validates :name, :picture, :rule, presence: true
+  validates :name, :picture, presence: true
+  validates :rule, presence: true, uniqueness: { scope: :value }
 end
