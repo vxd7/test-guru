9ba51a56f79d8b482b3d13afe1d0c70bbab62ba7
vxd7 <ykonpaku13@gmail.com>
2019-11-20 17:55:13 +0300

    Added timelimit to permit in tests_controller

--- app/controllers/admin/tests_controller.rb
+++ app/controllers/admin/tests_controller.rb
@@ -53,7 +53,7 @@ class Admin::TestsController < Admin::BaseController
   end
 
   def test_params
-    params.require(:test).permit(:title, :level, :category_id)
+    params.require(:test).permit(:title, :level, :category_id, :timelimit)
   end
 
   def find_test
